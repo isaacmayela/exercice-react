c7840b03c1ec9640006a9a5b5d22530c784d5e03
isaacmayela <isaacmayela27@gmail.com>
2023-11-14 16:03:20 +0000

    feat: my first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Board from './components/board'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <Board /> 
+    </main>
+  )
+}
+
+export default App
